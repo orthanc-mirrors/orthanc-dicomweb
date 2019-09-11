e4fabd9e0b27c8803e0be13320e01580dae7bc12
Sebastien Jodogne <s.jodogne@gmail.com>
2019-09-11 18:03:18 +0200

    QIDO-RS: Allow to query against a list of multiple values separated by commas

--- Plugin/QidoRs.cpp
+++ Plugin/QidoRs.cpp
@@ -180,6 +180,16 @@ namespace
           Orthanc::DicomTag tag(0, 0);
           if (OrthancPlugins::ParseTag(tag, key))
           {
+            // The following lines are new in DICOMweb > 1.0, and
+            // allow to query against a list of multiple values
+            // http://dicom.nema.org/MEDICAL/dicom/2019a/output/chtml/part18/sect_6.7.html#sect_6.7.1.1.1
+            boost::replace_all(value, "\\", "");  // Remove backslashes from source request
+
+            // Replace commas by backslashes
+            boost::replace_all(value, ",", "\\");
+            boost::replace_all(value, "%2c", "\\");
+            boost::replace_all(value, "%2C", "\\");
+            
             filters_[tag] = value;
           }
         }
