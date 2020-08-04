ca9bbbf848e4d8df205632f4339489a64d88be2d
Sebastien Jodogne <s.jodogne@gmail.com>
2020-08-04 13:50:57 +0200

    don't override the versions from CMakeLists in Holy Build Box

--- Resources/holy-build-box-internal.sh
+++ Resources/holy-build-box-internal.sh
@@ -38,9 +38,6 @@ cp -r /source/WebApplication /tmp/source-writeable/
 
 cmake /tmp/source-writeable \
       -DCMAKE_BUILD_TYPE=$1 -DSTATIC_BUILD=ON \
-      -DORTHANC_SDK_VERSION=1.7.0 \
-      -DORTHANC_FRAMEWORK_SOURCE=web \
-      -DORTHANC_FRAMEWORK_VERSION=1.7.2 \
       -DCMAKE_INSTALL_PREFIX=/target 
 
 make -j`nproc`
